057f89d6fc7ed64cd516d851fc7b6cc5abcc9a69
Matthew Sherborne <msherborne@gmail.com>
2016-06-05 20:15:18 +1000

    Added used header file

---
+++ src/HTTP_OutputToNet.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <boost/asio/write.hpp>
+#include <boost/iostreams/concepts.hpp>
+
+namespace RESTClient {
+
+namespace io = boost::iostreams;
+  
+/// This is a boost iostreams sink
+/// Everything written to it goes out on the network
+template <typename Connection> class OutputToNet : public io::sink {
+private:
+  Connection &connection;
+  asio::yield_context &yield;
+
+public:
+  OutputToNet(Connection &connection, asio::yield_context &yield)
+      : connection(connection), yield(yield) {}
+  std::streamsize write(const char *s, std::streamsize n) {
+    return asio::async_write(connection, asio::buffer(s, n),
+                             asio::transfer_at_least(n), yield);
+  }
+};
+
+} /* RESTClient */
